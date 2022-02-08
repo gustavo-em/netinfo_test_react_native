6b045e3ba2a430ac43b5ee2762560b4d3fa26175
gustavo-em <gustavo.emanuel01@outlook.com>
2022-02-08 14:08:56 -0300

    Implement NetInfo in App.js

--- App.js
+++ App.js
@@ -2,11 +2,21 @@ import React from 'react';
 import {
   SafeAreaView,Text
 } from 'react-native';
-
+import NetInfo from "@react-native-community/netinfo";
 const App = () => {
+  const [statusConnection, setStatusConnection] = React.useState(false)
+
+  React.useEffect(()=>{
+    NetInfo.addEventListener(state => {
+      console.log(state)
+      setStatusConnection(state)
+    });
+  }, [])
+
   return (
     <SafeAreaView>
-      <Text>Cleaning App.js</Text>
+      <Text>Connected: {statusConnection && statusConnection.isConnected ? 'Yes' : 'No'}</Text>
+      <Text>is Wifi: {statusConnection && statusConnection.isWifiEnabled ? 'Yes' : 'No'}</Text>      
     </SafeAreaView>
   );
 };
